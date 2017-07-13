90e1079d94751c1f9087706924e43e356803c0c5
Marcin Konopka <m.konopka@madkom.pl>
2017-07-13 11:24:26 +0200

    ServiceLoader

---
+++ src/Application/ServiceLoader.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkonopka
+ * Date: 13.07.2017
+ * Time: 11:06
+ */
+namespace Application;
+
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
+use Domain\Account;
+use Domain\Event\AccountCreated;
+use Domain\Event\MoneyAdded;
+use Domain\Event\MoneyWithdrawn;
+use Infrastructure\EventHandler\AccountCreatedEventHandler;
+use Infrastructure\EventHandler\MoneyAddedEventHandler;
+use Infrastructure\EventHandler\MoneyWithdrawnEventHandler;
+use Infrastructure\EventSourcedAccountRepository;
+use Infrastructure\RabbitMQClient;
+use Prooph\Common\Event\ProophActionEventEmitter;
+use Prooph\Common\Messaging\FQCNMessageFactory;
+use Prooph\Common\Messaging\NoOpMessageConverter;
+use Prooph\EventSourcing\EventStoreIntegration\AggregateTranslator;
+use Prooph\EventStore\Adapter\Doctrine\DoctrineEventStoreAdapter;
+use Prooph\EventStore\Adapter\PayloadSerializer\JsonPayloadSerializer;
+use Prooph\EventStore\Aggregate\AggregateRepository;
+use Prooph\EventStore\Aggregate\AggregateType;
+use Prooph\EventStore\EventStore;
+use Prooph\EventStoreBusBridge\EventPublisher;
+use Prooph\EventStoreBusBridge\TransactionManager;
+use Prooph\ServiceBus\CommandBus;
+use Prooph\ServiceBus\EventBus;
+use Prooph\ServiceBus\Plugin\Router\CommandRouter;
+use Prooph\ServiceBus\Plugin\Router\EventRouter;
+use Silex\Application;
+use Silex\Provider\SerializerServiceProvider;
+use Symfony\Component\Serializer\Normalizer\CustomNormalizer;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Normalizer\JsonSerializableNormalizer;
+
+class ServiceLoader
+{
+    /** @var  Application */
+    protected $app;
+
+    /**
+     * ServiceLoader constructor.
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function loadServices()
+    {
+        $this->app->register(new SerializerServiceProvider());
+        $this->app['serializer.normalizers'] = function () {
+            return array(new JsonSerializableNormalizer(), new CustomNormalizer(), new GetSetMethodNormalizer());
+        };
+
+        $this->app['db_connection'] = function () {
+            $config = new Configuration();
+            $connectionParams = array(
+                'dbname' => getenv('DB_NAME'),
+                'user' => getenv('DB_USER'),
+                'password' => getenv('DB_PASSWORD'),
+                'host' => getenv('DB_HOST'),
+                'port' => getenv('DB_PORT'),
+                'driver' => 'pdo_mysql',
+            );
+            return DriverManager::getConnection($connectionParams, $config);
+        };
+
+        $this->app['queue_client'] = function () {
+            return new RabbitMQClient(RABIT_HOST, RABIT_PORT, RABIT_USER, RABIT_PASS, RABIT_VHOST);
+        };
+
+        $this->app['event_bus'] = function () {
+            return new EventBus();
+        };
+        $this->app['event_store'] = function (Application $app) {
+            $eventStore = new EventStore (
+                new DoctrineEventStoreAdapter(
+                    $app['db_connection'],
+                    new FQCNMessageFactory(),
+                    new NoOpMessageConverter(),
+                    new JsonPayloadSerializer()
+                ),
+                new ProophActionEventEmitter()
+            );
+            (new EventPublisher($app['event_bus']))->setUp($eventStore);
+
+            return $eventStore;
+        };
+        $this->app['event_router'] = function (Application $app) {
+            $eventRouter = new EventRouter();
+            $eventRouter
+                ->route(AccountCreated::class)
+                ->to(new AccountCreatedEventHandler($app['queue_client'], $app['db_connection']));
+            $eventRouter
+                ->route(MoneyAdded::class)
+                ->to(new MoneyAddedEventHandler($app['queue_client'], $app['db_connection']));
+            $eventRouter
+                ->route(MoneyWithdrawn::class)
+                ->to(new MoneyWithdrawnEventHandler($app['queue_client'], $app['db_connection']));
+
+            return $eventRouter;
+        };
+
+        $this->app['repository.accounts'] = function (Application $app) {
+            return new EventSourcedAccountRepository(
+                new AggregateRepository(
+                    $app['event_store'],
+                    AggregateType::fromAggregateRootClass(Account::class),
+                    new AggregateTranslator()
+                )
+            );
+        };
+
+        $this->app['command_bus'] = function (Application $app) {
+            $commandBus = new CommandBus();
+            $transactionManager = new TransactionManager();
+            $transactionManager->setUp($app['event_store']);
+            $commandBus->utilize($transactionManager);
+
+            return $commandBus;
+        };
+
+        $this->app['command_router'] = function (Application $app) {
+            $commandRouter = new CommandRouter();
+            $commandRouter
+                ->route(CreateAccount::class)
+                ->to(new CreateAccountHandler($app['repository.accounts']));
+            $commandRouter
+                ->route(AddMoney::class)
+                ->to(new AddMoneyHandler($app['repository.accounts']));
+            $commandRouter
+                ->route(WithdrawMoney::class)
+                ->to(new WithdrawMoneyHandler($app['repository.accounts']));
+
+            return $commandRouter;
+        };
+
+        $this->app['event_router']->attach($this->app['event_bus']->getActionEventEmitter());
+        $this->app['command_router']->attach($this->app['command_bus']->getActionEventEmitter());
+    }
+}
--- web/index.php
+++ web/index.php
@@ -5,46 +5,12 @@
  * Date: 13.07.2017
  * Time: 08:11
  */
-use Application\AddMoney;
-use Application\AddMoneyHandler;
 use Application\CreateAccount;
-use Application\CreateAccountHandler;
-use Application\WithdrawMoney;
-use Application\WithdrawMoneyHandler;
-use Doctrine\DBAL\Configuration;
-use Doctrine\DBAL\DriverManager;
 use Domain\Account;
-use Domain\Event\AccountCreated;
-use Domain\Event\MoneyAdded;
-use Domain\Event\MoneyWithdrawn;
-use Infrastructure\EventHandler\AccountCreatedEventHandler;
-use Infrastructure\EventHandler\MoneyAddedEventHandler;
-use Infrastructure\EventHandler\MoneyWithdrawnEventHandler;
-use Infrastructure\EventSourcedAccountRepository;
-use Infrastructure\RabbitMQClient;
-use Prooph\Common\Event\ProophActionEventEmitter;
-use Prooph\Common\Messaging\FQCNMessageFactory;
-use Prooph\Common\Messaging\NoOpMessageConverter;
-use Prooph\EventSourcing\EventStoreIntegration\AggregateTranslator;
-use Prooph\EventStore\Adapter\Doctrine\DoctrineEventStoreAdapter;
-use Prooph\EventStore\Adapter\PayloadSerializer\JsonPayloadSerializer;
-use Prooph\EventStore\Aggregate\AggregateRepository;
-use Prooph\EventStore\Aggregate\AggregateType;
-use Prooph\EventStore\EventStore;
-use Prooph\EventStoreBusBridge\EventPublisher;
-use Prooph\EventStoreBusBridge\TransactionManager;
-use Prooph\ServiceBus\CommandBus;
-use Prooph\ServiceBus\EventBus;
-use Prooph\ServiceBus\Plugin\Router\CommandRouter;
-use Prooph\ServiceBus\Plugin\Router\EventRouter;
 use Rhumsaa\Uuid\Uuid;
 use Silex\Application;
-use Silex\Provider\SerializerServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\Normalizer\CustomNormalizer;
-use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
-use Symfony\Component\Serializer\Normalizer\JsonSerializableNormalizer;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/defines.php';
@@ -53,100 +19,10 @@ $app = new Application();
 
 $app['debug'] = true;
 
-$app->register(new SerializerServiceProvider());
-$app['serializer.normalizers'] = function () {
-    return array(new JsonSerializableNormalizer(), new CustomNormalizer(), new GetSetMethodNormalizer());
-};
+$serviceLoader = new \Application\ServiceLoader($app);
+$serviceLoader->loadServices();
 
-$app['db_connection'] = function() {
-    $config = new Configuration();
-    $connectionParams = array(
-        'dbname' => getenv('DB_NAME'),
-        'user' => getenv('DB_USER'),
-        'password' => getenv('DB_PASSWORD'),
-        'host' => getenv('DB_HOST'),
-        'port' => getenv('DB_PORT'),
-        'driver' => 'pdo_mysql',
-    );
-    return DriverManager::getConnection($connectionParams, $config);
-};
-
-$app['queue_client'] = function() {
-    return new RabbitMQClient(RABIT_HOST, RABIT_PORT, RABIT_USER, RABIT_PASS, RABIT_VHOST);
-};
-
-$app['event_bus'] = function () {
-    return new EventBus();
-};
-$app['event_store'] = function(Application $app) {
-    $eventStore = new EventStore (
-        new DoctrineEventStoreAdapter(
-            $app['db_connection'],
-            new FQCNMessageFactory(),
-            new NoOpMessageConverter(),
-            new JsonPayloadSerializer()
-        ),
-        new ProophActionEventEmitter()
-    );
-    (new EventPublisher($app['event_bus']))->setUp($eventStore);
-
-    return $eventStore;
-};
-$app['event_router'] = function(Application $app) {
-    $eventRouter = new EventRouter();
-    $eventRouter
-        ->route(AccountCreated::class)
-        ->to(new AccountCreatedEventHandler($app['queue_client'], $app['db_connection']));
-    $eventRouter
-        ->route(MoneyAdded::class)
-        ->to(new MoneyAddedEventHandler($app['queue_client'], $app['db_connection']));
-    $eventRouter
-        ->route(MoneyWithdrawn::class)
-        ->to(new MoneyWithdrawnEventHandler($app['queue_client'], $app['db_connection']));
-
-    return $eventRouter;
-};
-
-$app['repository.accounts'] = function(Application $app) {
-    return new EventSourcedAccountRepository(
-        new AggregateRepository(
-            $app['event_store'],
-            AggregateType::fromAggregateRootClass(Account::class),
-            new AggregateTranslator()
-        )
-    );
-};
-
-$app['command_bus'] = function(Application $app) {
-    $commandBus = new CommandBus();
-    $transactionManager = new TransactionManager();
-    $transactionManager->setUp($app['event_store']);
-    $commandBus->utilize($transactionManager);
-
-    return $commandBus;
-};
-
-$app['command_router'] = function(Application $app) {
-    $commandRouter = new CommandRouter();
-    $commandRouter
-        ->route(CreateAccount::class)
-        ->to(new CreateAccountHandler($app['repository.accounts']));
-    $commandRouter
-        ->route(AddMoney::class)
-        ->to(new AddMoneyHandler($app['repository.accounts']));
-    $commandRouter
-        ->route(WithdrawMoney::class)
-        ->to(new WithdrawMoneyHandler($app['repository.accounts']));
-
-    return $commandRouter;
-};
-
-$app['event_router']->attach($app['event_bus']->getActionEventEmitter());
-$app['command_router']->attach($app['command_bus']->getActionEventEmitter());
-
-
-
-$app->get('/accounts.{_format}', function(Application $app, Request $request){
+$app->get('/accounts.{_format}', function (Application $app, Request $request) {
     /** @var \Doctrine\DBAL\Connection $connection */
     $connection = $app['db_connection'];
 
@@ -159,18 +35,18 @@ $app->get('/accounts.{_format}', function(Application $app, Request $request){
 
 });
 
-$app->post('/accounts', function(Request $request, Application $app){
+$app->post('/accounts', function (Request $request, Application $app) {
     $currency = $request->get('currency', 'PLN');
 
     $accountId = Uuid::uuid4();
     $app['command_bus']->dispatch(new CreateAccount($accountId, $currency));
 
     return new Response('', 201, [
-        "Location" => "/accounts/" . (string) $accountId
+        "Location" => "/accounts/" . (string)$accountId
     ]);
 });
 
-$app->get('/accounts/{accountId}', function($accountId, Application $app, Request $request) {
+$app->get('/accounts/{accountId}', function ($accountId, Application $app, Request $request) {
     /** @var Account $account */
     $account = $app['repository.accounts']->get($accountId);
 
@@ -178,7 +54,7 @@ $app->get('/accounts/{accountId}', function($accountId, Application $app, Reques
     return new Response($app['serializer']->serialize($account, $format), 200, array(
         "Content-Type" => $request->getMimeType($format)
     ));
-})->convert('accountId', function($accountId){
+})->convert('accountId', function ($accountId) {
     return Uuid::fromString($accountId);
 });
 
